130f1def536144eb5c2bac91941d4b619fceb90c
Carson Long <carsonthelong@gmail.com>
2022-04-27 22:27:38 -0700

    Stop running osx script
    Most of these osx settings are better to set in the mac settings, or
    else they can be really hard to undo.

--- install.sh
+++ install.sh
@@ -19,14 +19,10 @@ echo
 echo "Open Spectacle"
 open /Applications/Spectacle.app
 
-echo "Configuring iTerm"
-# cp files/com.googlecode.iterm2.plist ~/Library/Preferences
-
 echo
 echo "Configuring neovim with Luan's config"
 git clone https://github.com/luan/nvim ${HOME}/.config/nvim
 
 source ${THIS_DIR}/scripts/git.sh
-source ${THIS_DIR}/scripts/osx.sh
 
 exit
