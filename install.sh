19fe0fefeb95c31dcd7a2f95f92e317af609e1d2
Carson Long <carsonthelong@gmail.com>
2022-04-27 22:27:00 -0700

    Move ruby and go language installs to asdf

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ THIS_DIR="$(dirname "$0")"
 HOMEBREW_BUNDLE_FILE=${THIS_DIR}/Brewfile
 
 source ${THIS_DIR}/scripts/homebrew.sh
+source ${THIS_DIR}/scripts/langs.sh
 source ${THIS_DIR}/scripts/zsh.sh
 
 echo
